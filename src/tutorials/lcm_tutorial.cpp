a59319d6003e5fb128521aa498a5e6798632c81a
Kevin French <fnivek@gmail.com>
2019-01-17 20:40:16 -0500

    TUTORIAL: Initilize lcm

--- src/tutorials/lcm_tutorial.cpp
+++ src/tutorials/lcm_tutorial.cpp
@@ -1,3 +1,9 @@
+// lcm cpp functionality
+#include <lcm/lcm-cpp.hpp>
+
+// Defines for lcm in this project
+#include <common/lcm_config.h>
+
 #include <ctime>
 #include <iostream>
 #include <sstream>
@@ -8,8 +14,14 @@
 
 int main(int argc, char ** argv)
 {
+    // Initilize lcm
+    lcm::LCM lcm(MULTICAST_URL);
+    if(!lcm.good())
+        return 1;
+
     while(1) {
         std::cout << "LCM Tutorial\n";
+        // Publish at 1 second intervals
         usleep(1000000);
     }
 }
