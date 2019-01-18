2b75435b7a2e0fdbd51aa53d4140ebe2ae668eca
Kevin French <fnivek@gmail.com>
2019-01-17 20:51:04 -0500

    TUTORIAL: Define an oled_message_t

--- src/tutorials/lcm_tutorial.cpp
+++ src/tutorials/lcm_tutorial.cpp
@@ -3,6 +3,10 @@
 
 // Defines for lcm in this project
 #include <common/lcm_config.h>
+// The message type for oled generated from lcmtypes/oled_message_t.lcm
+#include <lcmtypes/oled_message_t.hpp>
+// Code for producing timestamps
+#include <common/timestamp.h>
 
 #include <ctime>
 #include <iostream>
@@ -18,6 +22,15 @@ int main(int argc, char ** argv)
     lcm::LCM lcm(MULTICAST_URL);
     if(!lcm.good())
         return 1;
+
+    // Define an oled message
+    // we can look up the definition in lcmtypes/oled_message_t.lcm
+    oled_message_t msg;
+    // Fill in the timestamp
+    msg.utime = utime_now();
+    // Fill in the two lines of the oled
+    msg.line1 = "Hello";
+    msg.line2 = "world";
 
     while(1) {
         std::cout << "LCM Tutorial\n";
