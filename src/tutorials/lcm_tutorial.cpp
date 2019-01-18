6b24fd7b953d4fc2d46f7bc2fbf4b4c535f67b86
Kevin French <fnivek@gmail.com>
2019-01-17 20:35:35 -0500

    TUTORIAL: Start lcm tutorial

---
+++ src/tutorials/lcm_tutorial.cpp
@@ -0,0 +1,15 @@
+#include <ctime>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+
+#include <unistd.h>
+
+int main(int argc, char ** argv)
+{
+    while(1) {
+        std::cout << "LCM Tutorial\n";
+        usleep(1000000);
+    }
+}
