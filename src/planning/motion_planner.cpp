92365696ed0e52222778c6a49ef1f5616874d4b7
wuziyou <wuziyou@umich.edu>
2019-03-20 20:13:28 -0400

    planning up to date

--- src/planning/motion_planner.cpp
+++ src/planning/motion_planner.cpp
@@ -110,5 +110,5 @@ void MotionPlanner::setParams(const MotionPlannerParams& params)
 {
     searchParams_.minDistanceToObstacle = params_.robotRadius;
     searchParams_.maxDistanceWithCost = 10.0 * searchParams_.minDistanceToObstacle;
-    searchParams_.distanceCostExponent = 1.0;
+    searchParams_.distanceCostExponent = 1.2;
 }
